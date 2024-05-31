5a802bd4be0e216adb4581f97a0e5b4fbd20efb7
Siddharth <siddharth6527@gmail.com>
2024-06-01 03:26:27 +0530

    feat: Virtual Properties in Tours

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -9,6 +9,8 @@ router
   .route('/top-5-cheap')
   .get(tourController.aliasTopTours, tourController.getAllTours);
 
+router.route('/monthly-plan/:year').get(tourController.getMonthyPlan);
+
 router.route('/tours-stats').get(tourController.getTourStats);
 
 router
