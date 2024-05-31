cf3d4b5962d897a9874492a828f76681fc5f1afb
Siddharth <siddharth6527@gmail.com>
2024-05-31 18:55:17 +0530

    feat: Sorting in Tours

--- controllers/tourControllers.js
+++ controllers/tourControllers.js
@@ -3,21 +3,26 @@ const Tour = require('../models/tourModel');
 exports.getAllTours = async (req, res) => {
   try {
     // const queryObj = { ...req.query };
-    // 1. Filtering
+    // 1A. Filtering
     const queryObj = JSON.parse(JSON.stringify(req.query));
     const excludedFields = ['page', 'sort', 'limit', 'fields'];
     excludedFields.forEach((el) => {
       delete queryObj[el];
     });
 
-    // 2. Advanced Filtering
+    // 1B. Advanced Filtering
     let queryStr = JSON.stringify(req.query);
     queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
-    console.log(JSON.parse(queryStr));
 
-    const query = Tour.find(JSON.parse(queryStr));
+    let query = Tour.find(JSON.parse(queryStr));
 
-    console.log(req.query);
+    // 2. Sorting
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(',').join(' ');
+      query = query.sort(sortBy);
+    } else {
+      query = query.sort('-createdAt');
+    }
 
     // EXECUTE QUERY
     const tours = await query;
