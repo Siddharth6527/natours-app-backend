26235a217fb7bcb5ab101fa1f96631d68c32bb59
Siddharth <siddharth6527@gmail.com>
2024-06-02 15:50:06 +0530

    feat: Debugging with ndb

--- server.js
+++ server.js
@@ -9,17 +9,13 @@ const DB = process.env.DATABASE.replace(
   process.env.DATABASE_PASSWORD,
 );
 
-mongoose
-  .connect(DB, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-  })
-  .then(() => {
-    console.log('DB connection successful!');
-  });
+mongoose.connect(DB).then(() => {
+  console.log('DB connection successful!');
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
+
+//  SERVER
