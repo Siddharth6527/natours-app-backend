77a0a3f6b0d82993a90a0d0fd0a4a5bb84d51939
Siddharth <siddharth6527@gmail.com>
2024-06-02 09:24:48 +0530

    feat: Aggregation Middleware in Tours

--- models/tourModel.js
+++ models/tourModel.js
@@ -98,10 +98,12 @@ tourSchema.post(/^find/, function (docs, next) {
   next();
 });
 
-// tourSchema.pre('findOne', function (next) {
-//   this.find({ secretTour: { $ne: true } });
-//   next();
-// });
+// AGGREGATION MIDDLEWARE
+tourSchema.pre('aggregate', function (next) {
+  this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
+  console.log(this.pipeline());
+  next();
+});
 
 const Tour = mongoose.model('Tour', tourSchema);
 
