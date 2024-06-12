79fe8bc8744a357750cd1e1503933f5dacbdbb12
Siddharth <siddharth6527@gmail.com>
2024-06-12 21:56:20 +0530

    feat: Setting New Passwords

--- models/userModel.js
+++ models/userModel.js
@@ -57,6 +57,15 @@ userSchema.pre('save', async function (next) {
   next();
 });
 
+userSchema.pre('save', function (next) {
+  if (!this.isModified('password') || this.isNew) {
+    return next();
+  }
+
+  this.passwordChangedAt = Date.now() - 1000;
+  next();
+});
+
 // INSTANCE FUNCTION ~ available on all documents
 userSchema.methods.correctPassword = async function (
   candidatePassword,
@@ -86,8 +95,6 @@ userSchema.methods.createPasswordResetToken = function () {
     .createHash('sha256')
     .update(resetToken)
     .digest('hex');
-
-  console.log(resetToken, this.passwordResetToken);
 
   this.passwordResetExpires = Date.now() + 10 * 60 * 1000;
 
